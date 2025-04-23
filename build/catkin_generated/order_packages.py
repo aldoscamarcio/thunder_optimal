54d528df2a2fc046ed39edf74f6972d5cb006815
eligiomansi <73256942+eligiomansi@users.noreply.github.com>
2025-04-23 17:52:29 +0200

    Delete build directory

--- build/catkin_generated/order_packages.py
+++
@@ -1,5 +0,0 @@
-# generated from catkin/cmake/template/order_packages.context.py.in
-source_root_dir = '/home/frankino/Tesi/thunder_optimal/src'
-whitelisted_packages = ''.split(';') if '' != '' else []
-blacklisted_packages = ''.split(';') if '' != '' else []
-underlay_workspaces = '/home/frankino/Tesi/catkin_ws/devel;/opt/ros/noetic'.split(';') if '/home/frankino/Tesi/catkin_ws/devel;/opt/ros/noetic' != '' else []
